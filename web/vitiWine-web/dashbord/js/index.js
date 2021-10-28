761a9a475c704050b3a757298415d3a47c69098a
HannaSantos <hanna.santos@bandtec.com.br>
2021-10-27 22:57:07 -0300

    dashboard pronto

--- web/vitiWine-web/dashbord/js/index.js
+++ web/vitiWine-web/dashbord/js/index.js
@@ -2,10 +2,10 @@ var ctx = document.getElementById('teste').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: 'Temperatura',
-            data: [5, 2, 3],
+            data: [15, 19, 22, 23, 26, 30],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -38,7 +38,7 @@ var ctx = document.getElementById('pizza').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Perdas'],
+        labels: ['boa'],
         datasets: [{
             label: 'red',
             data: [5, 2, 3],
