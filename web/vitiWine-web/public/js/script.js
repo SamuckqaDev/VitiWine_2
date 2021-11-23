5fb4057acdb4482e5a8a5bea839e3990144fa98d
samuckqadev <samuel.tomimatu@bandtec.com.br>
2021-11-23 20:06:00 -0300

    API NODEJS - Login

--- web/vitiWine-web/public/js/script.js
+++ web/vitiWine-web/public/js/script.js
@@ -2,7 +2,7 @@ function cadastrar() {
   var nomeVar = ipt_nome.value;
   var cnpjVar = ipt_cnpj.value;
   var celularVar = ipt_tele.value;
-  var emailVar = ipt_email1.value ;
+  var emailVar = ipt_email1.value;
   var senhaVar = ipt_senha1.value;
 
   if (
@@ -45,7 +45,7 @@ function cadastrar() {
       nomeServer: nomeVar,
       cnpjServer: cnpjVar,
       celularServer: celularVar,
-      emailServer: emailVar,
+      emailServer: emailVar.toLowerCase(),
       senhaServer: senhaVar,
     }),
   })
@@ -61,6 +61,70 @@ function cadastrar() {
     })
     .catch(function (resposta) {
       console.log(`#ERRO: ${resposta}`);
+    });
+
+  return false;
+}
+
+/****************************LOGIN*****************************/
+function entrar() {
+  var emailVar = ipt_email.value;
+  var senhaVar = ipt_senha.value;
+
+  console.log("FORM LOGIN: ", emailVar);
+  console.log("FORM SENHA: ", senhaVar);
+
+  // TODO: VERIFICAR AS VALIDAÇÕES QUE ELES ESTÃO APRENDENDO EM ALGORITMOS
+  if (emailVar == "" || senhaVar == "") {
+    window.alert("Preencha todos os campos para prosseguir!");
+
+    return false;
+  }
+
+  if (emailVar.indexOf("@") == -1 || emailVar.indexOf(".com") == -1) {
+    window.alert("Ops, e-mail inválido! Verifique e tente novamente.");
+
+    return false;
+  }
+
+  fetch("/empresa/autenticar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      emailServer: emailVar.toLowerCase(),
+      senhaServer: senhaVar,
+    }),
+  })
+    .then((resposta) => {
+      console.log("ESTOU NO THEN DO entrar()!");
+
+      if (resposta.ok) {
+        console.log(resposta);
+
+        resposta.json().then((json) => {
+          console.log(json);
+          console.log(JSON.stringify(json));
+
+          sessionStorage.EMAIL_USUARIO = json.email;
+          sessionStorage.NOME_USUARIO = json.Nome_empresa;
+        
+          setTimeout(() => {
+            window.location = "dashbord/index.html";
+          }, 1000); // apenas para exibir o loading
+        });
+      } else {
+        console.log("Houve um erro ao tentar realizar o login!");
+
+        resposta.text().then((texto) => {
+          console.error(texto);
+        });
+        alert("Senha ou email Incorretos, por favor verifique ! ");
+      }
+    })
+    .catch((erro) => {
+      console.log(erro);
     });
 
   return false;
